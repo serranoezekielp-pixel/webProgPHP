4d8f7e8f8549002daab281a55a436417e15570c2
serranoezekielp-pixel <serranoezekielp@yahoo.com>
2026-06-04 17:25:56 -0400

    Add files via upload

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+
+$api_key = "live_JGu1h3T750jDySN7cEmRs5hcJI87qdKH4nZ5TmbljUtEOo0cpOVCsNe0aBXpgp73";
+
+$api_url = "https://api.thedogapi.com/v1/images/search";
+
+?>
---
+++ index.php
@@ -0,0 +1,56 @@
+<?php
+require_once("config.php");
+
+$options = [
+    "http" => [
+        "header" => "x-api-key: $api_key"
+    ]
+];
+
+$context = stream_context_create($options);
+
+$response = file_get_contents($api_url, false, $context);
+$data = json_decode($response, true);
+
+$dog = $data[0];
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="A simple web application that generates random dog pictures using The Dog API.">
+    <meta name="
+    <title>Random Dog Generator</title>
+
+    <link rel="stylesheet" href="CSS/style.css">
+</head>
+
+<body>
+
+<header>
+    <h1>Random Dog Pictures!</h1>
+</header>
+
+<main>
+
+<section class="card">
+
+    <h2>Press the button to shuffle!</h2>
+
+    <img src="<?php echo $dog['url']; ?>" alt="Random Dog Pictures">
+
+    <button onclick="location.reload()">Generate a new picture</button>
+
+</section>
+
+</main>
+
+<footer>
+    <p>PHP Assignment One API Integrationt</p>
+</footer>
+
+</body>
+</html>
